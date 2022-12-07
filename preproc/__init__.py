d1332d045c597cec98a851ba1c64261292ddd1a8
Orlando Alexander <67097862+orlandoalexander@users.noreply.github.com>
2022-12-07 13:48:55 -0600

    Update __init__.py

---
+++ preproc/__init__.py
@@ -0,0 +1,10 @@
+import sys, os
+sys.path.append(os.path.abspath(".."))
+
+try:
+  import google.colab  # so I only do it when I'm on google colab
+  sys.path.insert(0, "/content/preproc")
+except:
+  pass
+
+import aa
