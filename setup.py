8328262f5b8ac65a7d665e923a73790689b97e98
orlandoalexander <itsorlando@outlook.com>
2022-12-06 23:21:35 -0600

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(name='preproc',
+      description="preprocess images and convert to numpy arrays",
+      packages=find_packages(), # find packages automatically
+      install_requires=requirements) # install dependencies when install package
