098dd7f83caecf93c99a8f32c5f2741433c44ab0
Nigel Wright <nigeljwright22@gmail.com>
2022-12-08 20:05:30 -0600

    did some things

--- Models/attr_model.py
+++ Models/attr_model.py
@@ -1,9 +1,8 @@
-import numpy as np
-import pandas as pd
-
+import gc
+from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
 
-from keras.layers import Input, Dense, Flatten
+from keras.layers import Input, Dense, Flatten, GlobalAveragePooling2D
 from keras import Sequential
 from keras.metrics import Precision, Recall
 from keras.optimizers import Adam
@@ -25,33 +24,32 @@ from keras.applications import efficientnet
 
 class AttrModel:
     def __init__(self, attribute, model, input_shape, final_layer_neurons,
-                 X_train, X_test,
+                 pooling, X_train, X_test,
                  y_train, y_test, **kwargs):
         self.attribute = attribute
         self.model = model.lower()
         self.input_shape = input_shape
         self.final_layer_neurons = final_layer_neurons
         self.kwargs = kwargs
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = y_test
-
         self.cat_nums = {
             'design': 7,
             'sleeves': 3,
-            'length': 2,
+            'length': 1,    # binary classification
             'neckline': 4,
             'fabric': 6,
             'fit': 3
         }
-
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(
+            X_train, y_train, test_size=.5
+        )
+        self.X_test = X_test
+        self.y_test = y_test
+        del X_train, X_val, X_test, y_train, y_val, y_test
+        gc.collect()
         self.cat_num = self.cat_nums[self.attribute]
         self.activation = 'sigmoid' if self.attribute == 'length' else 'softmax'
-        self.loss = 'binary_crossentropy' if self.attribute == 'length' else 'categorical_crossentropya'
-
+        self.loss = 'binary_crossentropy' if self.attribute == 'length' else 'categorical_crossentropy'
         self.model = self.instantiate_model()  # calling the function below
-
 
     def instantiate_model(self):
         # import ipdb;
@@ -74,37 +72,37 @@ class AttrModel:
                   inception, resnet, mobilenet, efficientnet''')
 
         base_model.trainable = False    # freeze layers
-
-        flat = Flatten()
-        dense = Dense(units=self.final_layer_neurons, activation='relu')
-        prediction = Dense(units=self.cat_num, activation=self.activation)
-        model = Sequential([base_model, flat, dense, prediction])
-
-        model.compile(loss=self.loss, optimizer='adam',
+        pool = GlobalAveragePooling2D() # add pooling layer -> have tried Flatten() here too
+        dense = Dense(units=self.final_layer_neurons, activation='relu') # add dense layer
+        prediction = Dense(units=self.cat_num, activation=self.activation) # prediction layer
+        model = Sequential([base_model, pool, dense, prediction])   # add layers to model
+        model.compile(loss=self.loss, optimizer='adam',             # compile model
                       metrics=['accuracy', Precision(), Recall()])
-
         return model
 
-    def train(self, epochs=20, batch_size=32):
+    def train(self):
         # import ipdb
         # ipdb.set_trace()
         self.model.history = self.model.fit(
             self.X_train, self.y_train,
-            epochs=epochs, batch_size=batch_size,
+            epochs=self.epochs, batch_size=self.batch_size,
             validation_split=0.2, verbose=1,
             callbacks=[EarlyStopping(patience=2, restore_best_weights=True)])
         return self.model.history.history
 
-    def finetune(self, epochs=20, callbacks=[EarlyStopping(patience=3)],
-                 batch_size=32, loss=self.loss,
-                 metrics=['accuracy', Precision(), Recall()],
-                 new_data=None):
-        self.model.base_model.trainable = True     # unfreeze layers, then compile to save changes
+    def finetune(self,
+                 metrics=['accuracy', Precision(), Recall()]):
+        self.model.trainable = True     # unfreeze layers, then compile to save changes
         self.model.compile(
             optimizer=Adam(1e-5),                  # Very low learning rate
-            loss=loss,
+            loss=self.loss,
             metrics=metrics)
-        self.model.fit(new_data, epochs=epochs, callbacks=..., validation_data=...)
+        self.model.fit(self.X_val, self.y_val,
+                       epochs=self.epochs, batch_size=self.batch_size,
+                       callbacks=[EarlyStopping(patience=2, monitor='val_accuracy',
+                                                restore_best_weights=True)],
+                       validation_split=0.2)
+        return self.model.history
 
 
     def evaluate(self):
