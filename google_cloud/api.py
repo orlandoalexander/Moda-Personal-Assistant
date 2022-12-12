4511f36a2d62a4633df1299c65c451ee629dcde7
orlandoalexander <itsorlando@outlook.com>
2022-12-12 01:53:28 -0600

    Google cloud

--- google_cloud/api.py
+++ google_cloud/api.py
@@ -1,29 +1,56 @@
 import requests
 import pandas as pd
 import numpy as np
+from google.cloud import storage
+
+API_KEY = 'e6d0546079msh1698fc71e2b4d4ep1e1163jsnecaf48e249c5'
+BUCKET_NAME = 'moda-data'
+FILE_NAME = 'moda_asos_data.cvs'
 
 class API():
-    def __init__(self, API_KEY) -> None:
+    def __init__(self, API_KEY, BUCKET_NAME, FILE_NAME) -> None:
         self._url_categories = "https://asos2.p.rapidapi.com/categories/list"
         self._url_products = "https://asos2.p.rapidapi.com/products/v2/list"
-        self._querystring_categories = {"country":"US","lang":"en-US"}
         self._headers = {"X-RapidAPI-Key": API_KEY, "X-RapidAPI-Host": "asos2.p.rapidapi.com"}
+        self._bucket_name = BUCKET_NAME
+        self._file_name = FILE_NAME
 
     def get_products(self):
         self._cats = self._get_categories()
         products = []
-        for cat in self._cats:
-            querystring = {f"store":"US","offset":"0","categoryId":{cat},"limit":"48","sort":"freshness","lang":"en-UK"}
-            response = requests.request("GET", self._url, headers=self._headers, params=self._querystring).json()
-            products.append(response)
+        for cat in self._cats[:2]:
+            querystring = {f"store":"US","offset":0,"categoryId":{cat},"limit":48,"sort":"freshness","lang":"en-UK"}
+            response = requests.request("GET", self._url_products, headers=self._headers, params=querystring).json()
+            product_count = response['itemCount']
+            query_vals = [(48,48)]
+            while sum(query_vals[-1]) < product_count:
+                diff = (product_count-(query_vals[-1][0]+48))
+                if diff < 48:
+                    n = diff%48
+                query_vals.append((query_vals[-1][0]+48,n))
+            query_vals.pop(0)
+            for query_val in query_vals:
+                querystring = {f"store":"US","offset":query_val[0],"categoryId":{cat},"limit":query_val[1],"sort":"freshness","lang":"en-UK"}
+                response = requests.request("GET", self._url_products, headers=self._headers, params=querystring).json()
+            product = [{'id':product['id'],'name':product['name'],'price_current':product['price']['current']['value'],'price_previous':product['price']['previous']['value'],'marked_down':product['price']['isMarkedDown'],'outlet':product['price']['isOutletPrice'],'selling_fast':product['isSellingFast'],'brand':product['brandName'], 'url':'https://www.asos.com/us/'+product['url'], 'url_image': [product['imageUrl']]+[product['imageUrl'][:product['imageUrl'].index(f"{product['id']}-")]+f"{product['id']}-{num}" for num in range(2,5)]} for product in response['products']]
+            products.extend(product)
+        self._df = pd.DataFrame(products)
+        self._save_to_bq()
 
     def _get_categories(self):
-        response = requests.request("GET", self._url, headers=self._headers, params=self._querystring_categories).json()
+        querystring = {"country":"US","lang":"en-US"}
+        response = requests.request("GET", self._url_categories, headers=self._headers, params=querystring).json()
         brands = np.array(response['brands'][0:4]) # get all clothing brands
         cats = np.array([cat['link']['categoryId'] for brand in brands for cat in brand['children']])
         return cats
 
+    def _save_to_bq(self):
+        client = storage.Client(project='lewagonbootcamp-371116')
+        bucket = client.bucket(self._bucket_name)
+        blob = bucket.blob(self._file_name)
+        blob.upload_from_string(self._df.to_csv(index = False),content_type = 'csv')
 
 
-
-    API_KEY = 'e6d0546079msh1698fc71e2b4d4ep1e1163jsnecaf48e249c5'
+def main(data, context):
+  api_asos = API(API_KEY, BUCKET_NAME, FILE_NAME)
+  api_asos.get_products()
